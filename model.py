8abc2e8b797f88e43dfc5583b1031d087379f91c
NabinSharma10 <60011231@kmitl.ac.th>
2020-09-22 08:30:19 +0700

    Quantization added

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 import os
 import random
 import cv2
-
+import sklearn.cluster import MiniBatchKmeans # Make sure you have sklearn--> pip install sklearn
 class modelImage:
 	def __init__(self):
 		self.clusters = []
@@ -25,6 +25,7 @@ class modelImage:
 		return imageDict
 
 	def request_dissimilar_images(self, clusterName):
+
 		#[ (imagelocation, percentageDissimilar), (imagelocation, percentageDissimilar), (imagelocation, percentageDissimilar)... ]
 		pass
 
@@ -32,8 +33,28 @@ class modelImage:
 
 
         def brightness(self, image, alpha=1.0, beta=2.0):
+
             self.image = cv2.imread(image)
 
             result = cv2.addWeighted(image,alpha,np.zeros(image.shape,image.dtype),0,beta)
 
             return result
+
+        def quant(self,image,cluster):
+
+            #Import of from sklearn.cluster import MiniBatchKmeans needed
+            from sklearn.cluster import MiniBatchKmeans
+            self.image = cv2.imread(image)
+            (h,w) = image.shape(:2)
+            image = cv2.cvtColor((image.shape[0] * image.shape[1],3))
+            
+            clt = MiniBatchKMeans(n_clusters = cluster)
+            quant = clt.cluster_centers_.astype("uint8")[labels]
+
+            quant = quant.reshape((h,w,3))
+            image = image.reshape((h,w,3))
+
+            quantized_image = cv2.hstack([image,quant])
+
+            return quantized_image
+
